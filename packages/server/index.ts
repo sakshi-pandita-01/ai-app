3f0c11c60755f506e83ced77a94b4e9fd0038dc8
sak13301 <sak13301@esri.com>
2025-10-24 15:38:45 +0530

    open ai key added

---
+++ packages/server/index.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import express from "express";
+
+import type { Request, Response } from "express";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from the server!" + process.env.OPENAI_KEY);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
